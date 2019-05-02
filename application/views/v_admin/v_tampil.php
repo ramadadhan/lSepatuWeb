b24f66f1e211ac1a11d97f7f5bede07f8467bcbc
Susilawati1911 <45746385+Susilawati1911@users.noreply.github.com>
2019-05-02 19:55:20 +0700

    a

---
+++ application/views/v_admin/v_tampil.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!--	<title>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</title> -->
+</head>
+<body>
+<div class="table-responsive">
+
+	<center><?php echo anchor('admin/tambah','Tambah Data'); ?></center>
+	<table  style="margin:20px auto;" border="1"  width="100%">
+		<tr>
+			<th>No</th>
+			<th>Tanggal Daftar</th>
+			<th>Nama</th>
+			<th>Username</th>
+			<th>Password</th>
+            <th>Level</th>
+			<th>Status</th>
+			<th>Action</th>
+		</tr>
+		<?php 
+		$no = 1;
+		foreach($tbl_admin as $a){ 
+		?>
+		<tr>
+			<td><?php echo $no++ ?></td>
+			<td><?php echo $a->admin_tanggal ?></td>
+			<td><?php echo $a->admin_nama ?></td>
+			<td><?php echo $a->admin_username ?></td>
+			<td><?php echo password_hash("secret password", PASSWORD_DEFAULT) ?></td>
+            <td><?php echo $a->admin_level ?></td>
+			<td><?php echo $a->admin_status ?></td>
+			<td>
+			      <?php echo anchor('admin/edit/'.$a->admin_id,'Edit'); ?>
+				  
+                              <?php echo anchor('admin/hapus/'.$a->admin_id,'Hapus'); ?>
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+	</div>
+</body>
+</html>
