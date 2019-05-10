e0c3b9e1d01910fc84fc9d23ef3361506bc78ac0
Yadri Amzal <43919037+yamzal@users.noreply.github.com>
2019-05-10 10:21:37 +0700

    auth

---
+++ application/controllers/admin/Auth.php
@@ -0,0 +1,136 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        
+    }
+    
+    public function index()
+    {
+   
+        //form validation for field
+        $this->form_validation->set_rules('user_username','username', 'trim|required|valid_email');
+        $this->form_validation->set_rules('user_password', 'password','trim|required');
+        
+        if($this->form_validation->run() == false)
+        {        
+            $data['title'] = 'Login Page';
+            $this->load->view('auth/templates/auth_header',$data);
+            $this->load->view('auth/v_login');
+            $this->load->view('auth/templates/auth_footer');
+        } else {
+            
+            $this->_login();            
+        }                        
+    }
+    
+    private function _adminLogin()
+    {   
+        //admin var
+        $username = $this->input->post('admin_username');
+        $password = $this->input->post('admin_password');
+                        
+        $admin = $this->M_auth->adminLogin;
+        
+        if($admin){
+            
+            if($admin['admin_status'] == 1) {
+            
+                if(password_verify($password, $admin['admin_password'])){
+                    $data = [   
+                        'admin_username' => $admin['admin_username'],
+                        'admin_level' => $admin['admin_level']
+                    ];
+                    $this->session->set_userdata($data);
+                        if($admin['admin_level'] == 1){
+                            redirect('admin');
+                            } else {
+                                redirect('pegawai');
+                        }
+                    
+                    } else {
+                        //wrong password
+                        $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">Password salah!</div>');
+                        redirect('auth');       
+                    }
+                
+                } else {
+                    //if not activated
+                    $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">username belum diaktivasi!</div>');
+                        redirect('auth');     
+                }
+
+            } else {
+                //not registered
+                $this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">username tidak terdaftar!</div>');
+                redirect('auth');    
+
+            }
+        
+    }
+    
+    public function adminRegistrasi()
+    {
+         
+        $this->form_validation->set_rules('admin_nama', 'Nama', 'required|trim');
+        $this->form_validation->set_rules('admin_username', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+                                           'is_unique' => 'Email ini sudah digunakan!'
+                                            ]);
+        $this->form_validation->set_rules('admin_password1', 'Password', 'required|trim|min_length[3]|matches[admin_password2]',[
+                                            'matches' => 'password tidak cocok!',
+                                            'min_length' => 'password terlalu pendek!'                                            
+                                            ]);
+        $this->form_validation->set_rules('admin_password2', 'Password', 'required|trim|matches[admin_password1]');
+        
+        if( $this->form_validation->run() == false)
+        
+        {             
+            $data['title'] = 'LSepatu';
+            $this->load->view('v_auth/templates/auth_header',$data) ;
+            $this->load->view('auth/registration');
+            $this->load->view('v_auth/templates/auth_footer');            
+        } else {            
+            $email = $this->input->post('admin_username','true');
+            //if according to requirement this will input data to database
+            $data = [
+                    'admin_tanggal' => time(),
+                    'admin_nama' => htmlspecialchars($this->input->post('admin_nama',true)),
+                    'admin_username' => htmlspecialchars($email),                
+                    'admin_password' => password_hash($this->input->post('admin_password1'), PASSWORD_DEFAULT),
+                    'admin_level' => 2,
+                    'admin_status' => 0            
+            ];
+            
+            $token = base64_encode(random_bytes(32));
+            $admin_token = [
+                
+                'admin_username' => $email,
+                'admin_token' => $token
+                
+                //date created limit or time for expired email verification 
+            ];
+            
+            $this->M_auth->adminRegistrasi();
+            $this->M_auth->adminVerifikasi();
+
+            $this->_sendEmail();
+
+            $this->session->set_flashdata('message','<div class="alert alert-success" role="alert">
+            akun berhasil dibuat, mohon aktivasi terlebih dahulu</div>');
+            redirect('auth');
+        }
+        
+    }
+    
+    
+    
+
+    
+    
+    
+}
