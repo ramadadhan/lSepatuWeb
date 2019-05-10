88f14c326af3d3db69a38ef4ef6c975aff3e9ef3
Yadri Amzal <43919037+yamzal@users.noreply.github.com>
2019-05-10 13:57:34 +0700

    Update v_registrasi.php

--- application/views/auth/v_registrasi.php
+++ application/views/auth/v_registrasi.php
@@ -17,37 +17,43 @@
                       <!--to set form error in midle position-->
                       <?= form_error('name','<small class="text-danger ml-3">','</small>'); ?>
                   </div>
+                  
                   <div class="form-group">
                       <!--add set value to repopulating the form for user which input wrong format on form-->
                       <input type="text" class="form-control form-control-user" id="admin_username" name="admin_username" placeholder="Username" value="<?= set_value('admin_username'); ?>">
                       <!--to set form error in midle position-->
                       <?= form_error('name','<small class="text-danger ml-3">','</small>'); ?>
                   </div>
+                  
                   <div class="form-group">
                       <input type="text" class="form-control form-control-user" id="admin_email" name="admin_email" placeholder="Email Address" value="<?= set_value('admin_email'); ?>">
                       <!--to set form error in midle position-->
                        <?= form_error('email','<small class="text-danger ml-3">','</small>'); ?>
                   </div>
+                  
                   <div class="form-group row">
                       <div class="col-sm-6 mb-3 mb-sm-0">
                           <input type="password" class="form-control form-control-user" id="admin_password1" name="admin_password1" placeholder="Password">
                            <!--to set form error in midle position-->
                            <?= form_error('admin_password1','<small class="text-danger ml-3">','</small>'); ?>
                       </div>
+                      
                       <div class="col-sm-6">
                           <input type="password" class="form-control form-control-user" id="admin_password2" name="admin_password2" placeholder="Repeat Password">
                       </div>
                   </div>
+                  
                   <button type="submit" class="btn btn-primary btn-user btn-block">
                       Register Account
                   </button>                                                            
               </form>
               <hr>
+              
               <div class="text-center"> 
                   <a class="small" href="<?= base_url('');?>">Forgot Password?</a>
               </div>
               <div class="text-center">
-                  <a class="small" href="<?= base_url('admin/auth/adminregistrasi'); ?>">Already have an account? Login!</a>
+                  <a class="small" href="<?= base_url('admin/auth'); ?>">Already have an account? Login!</a>
               </div>
           </div>
       </div>
