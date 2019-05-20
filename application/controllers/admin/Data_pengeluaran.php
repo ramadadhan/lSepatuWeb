a2cf5cf99615ae2cc2c17ef9f43c3d6b96e86a8f
Ramadhan Ibnu Umam <ramaibnu83@gmail.com>
2019-05-20 09:58:41 +0700

    pengeluaran fix kurng update

--- application/controllers/admin/Data_pengeluaran.php
+++ application/controllers/admin/Data_pengeluaran.php
@@ -10,7 +10,7 @@
     
     
         function index () {
-            $data['pengeluaran'] = $this->m_pengeluaran->tampil_paket()->result();    
+            $data['tbl_pengeluaran'] = $this->m_pengeluaran->tampil_paket()->result();    
             $this->load->view('admin/v_pengeluaran/v_data_pengeluaran', $data);
         }
 
@@ -31,39 +31,38 @@
             redirect('admin/data_pengeluaran');
         }
     
-        function hapus($pengaluaran_id){
-            $where = array('pengeluaran_id' => $pengaluaran_id);
+        function hapus($pengeluaran_id){
+            $where = array('pengeluaran_id' => $pengeluaran_id);
             $this->m_pengeluaran->hapus_data($where,'tbl_pengeluaran');
             redirect('admin/data_pengeluaran/index');
         }
 
-        function edit($pengaluaran_id){
-            $where = array('pengeluaran_id' => $pengaluaran_id);
+        function edit($pengeluaran_id){
+            $where = array('pengeluaran_id' => $pengeluaran_id);
             $data['tbl_pengeluaran'] = $this->m_pengeluaran->edit_data($where,'tbl_pengeluaran')->result();
             $this->load->view('admin/v_pengeluaran/v_edit',$data);
         }
 
         function update(){
-            $pengaluaran_id = $this->input->post('pengeluaran_id');
+            $pengeluaran_id = $this->input->post('pengeluaran_id');
+            $pengeluaran_tanggal = $this->input->post('pengeluaran_tanggal');
             $pengeluaran_nama = $this->input->post('pengeluaran_nama');
             $pengeluaran_harga = $this->input->post('pengeluaran_harga');
             $pengeluaran_keterangan = $this->input->post('pengeluaran_keterangan');
         
             $data = array(
+                'pengeluaran_tanggal' => $pengeluaran_tanggal,
                 'pengeluaran_nama' => $pengeluaran_nama,
                 'pengeluaran_harga' => $pengeluaran_harga,
                 'pengeluaran_keterangan' => $pengeluaran_keterangan
             );
         
             $where = array(
-                'pengeluaran_id' => $pengaluaran_id
+                'pengeluaran_id' => $pengeluaran_id
             );
         
             $this->m_pengeluaran->update_data($where,$data,'tbl_pengeluaran');
-            redirect('data_pengeluaran/index');
+            redirect('admin/data_pengeluaran/index');
         }
-
-
     }
-
  ?>
